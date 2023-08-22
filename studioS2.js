fc8d85d021a5e75eabd8544242e9250ca92d9f8a
Hamzi Roslan <125324896+zzimha@users.noreply.github.com>
2023-08-22 16:34:30 +0800

    Update studioS2.js

--- studioS2.js
+++ studioS2.js
@@ -32,6 +32,21 @@ function combo_price(x) {
 
 combo_price(4); 
 
+// Question 6
+function add_to_order(order, combo) {
+  return order * 10 + combo;
+}
+
+
+// Question 7
+function last_combo(order) {
+  return order % 10;
+}
+
+// Question 8
+function other_combos(order) {
+  return math_trunc(order / 10);
+}
 
     
 
